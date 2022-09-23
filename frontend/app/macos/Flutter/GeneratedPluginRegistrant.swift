4f5a1f1af129f6e2e2de116d863c731184adabde
Ankur Soni <ankur.soni@outlook.com>
2022-09-23 21:13:33 +1000

    Feature/prototype1 (#3)

--- frontend/app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend/app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import flutter_secure_storage_macos
-import flutter_web_auth
+import flutter_web_auth_2
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStorageMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageMacosPlugin"))
-  FlutterWebAuthPlugin.register(with: registry.registrar(forPlugin: "FlutterWebAuthPlugin"))
+  FlutterWebAuth2Plugin.register(with: registry.registrar(forPlugin: "FlutterWebAuth2Plugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
